e8ca17654aefd8e7fedf9449abca81cfd1536539
bhanu <kondabathulabhanu@gmail.com>
2024-10-07 11:33:13 +0530

    kbc_quiz_game

---
+++ src/App.js
@@ -0,0 +1,30 @@
+import { useState } from "react";
+import PlayingGame from "./pages/PlayingGame";
+import StartGame from "./pages/StartGame";
+
+// 2 states:- "playing", "start"
+function App() {
+  const [gameState, setGameState] = useState("start");
+  const [userName, setUserName] = useState(null);
+
+  const playGameHandler = (name) => {
+    setGameState("playing");
+    setUserName(name);
+  };
+
+  const startGameHandler = () => {
+    setGameState("start");
+  };
+
+  return (
+    <>
+      {gameState === "start" && <StartGame onPlayGame={playGameHandler} />}
+      {gameState === "playing" && (
+        <PlayingGame onStartGame={startGameHandler} />
+      )}
+       <p>{userName}</p>
+    </>
+  );
+}
+
+export default App;
---
+++ src/pages/PlayingGame.js
@@ -0,0 +1,73 @@
+import React, { useEffect, useState } from "react";
+import classes from "./PlayingGame.module.css";
+import Question from "../components/Question";
+import questions from "../assets/questions";
+import useSound from "use-sound";
+import play from "../assets/sounds/play.mp3";
+
+const PlayingGame = (props) => {
+  const [currentQuestionNo, setCurrentQuestionNo] = useState(0);
+
+  const [gameInProgress, setGameInProgress] = useState(true);
+
+  const [playSound] = useSound(play);
+  const [correctAnsCount,setCurrectAnsCount]=useState(0);
+
+  useEffect(() => {
+    playSound();
+  }, [playSound]);
+
+ 
+
+  const onWinningTheGame = () => {
+
+    setGameInProgress(false);
+  };
+
+  const answerHandler = (isAnswerCorrect) => {
+    if (!isAnswerCorrect) {
+      // end the game
+      setGameInProgress(false);
+    }
+    if (isAnswerCorrect) {
+      if (currentQuestionNo !== questions.length - 1) {
+        // proceed to next question
+        setCurrectAnsCount((prev)=>prev+1);
+        setCurrentQuestionNo((prev) => prev + 1);
+      } else {
+        // last question successfully answered
+        onWinningTheGame();
+      }
+    }
+  };
+
+  const timeOverHandler = () => {
+    setGameInProgress(false);
+  };
+
+  return (
+    <div className={classes.container}>
+      <div className={classes.playing}>
+        {gameInProgress ? (
+          <Question
+            currentQuestionNo={currentQuestionNo}
+            isAnswerCorrect={answerHandler}
+            onTimeOver={timeOverHandler}
+          />
+        ) : (
+          <>
+            <span className={classes.prize}>
+              You have won {correctAnsCount} points
+            </span>
+            <button onClick={props.onStartGame} className={classes.btn}>
+              Play again
+            </button>
+          </>
+        )}
+      </div>
+
+    </div>
+  );
+};
+
+export default PlayingGame;
